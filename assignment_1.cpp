c5034c54af7b09d39915d71a4683cbf73ae57af5
Thijs Vijgeboom <avijgeboom@hotmail.com>
2022-09-21 18:49:11 +0200

    Ik ben geniaal

--- assignment_1.cpp
+++ assignment_1.cpp
@@ -10,6 +10,60 @@
         const int currentMonth = 9;
         const int currentDay = 21;
  
+        const int calYear = 1901;
+        const int calMonth = 1;
+        const int calDay = 1;
+
+        const int lJan = 31;
+        const int lFeb = 28;
+        const int lMar = 31;
+        const int lApr = 30;
+        const int lMay = 31;
+        const int lJun = 30;
+        const int lJul = 31;
+        const int lAug = 31;
+        const int lSep = 30;
+        const int lOct = 31;
+        const int lNov = 30;
+
+        int calNum = calYear*365 + calYear/4 - calYear/100 + calYear/400 + calDay;
+
+        if (calMonth == 2) {
+            calNum = calNum + lJan;
+        }
+        if (calMonth == 3) {
+            calNum = calNum + lJan + lFeb;
+        }
+        if (calMonth == 4) {
+            calNum = calNum + lJan + lFeb + lMar;
+        }
+        if (calMonth == 5) {
+            calNum = calNum + lJan + lFeb + lMar + lApr;
+        }
+        if (calMonth == 6) {
+            calNum = calNum + lJan + lFeb + lMar + lApr + lMay;
+        }
+        if (calMonth == 7) {
+            calNum = calNum + lJan + lFeb + lMar + lApr + lMay + lJun;
+        }
+        if (calMonth == 8) {
+            calNum = calNum + lJan + lFeb + lMar + lApr + lMay + lJun + lJul;
+        }
+        if (calMonth == 9) {
+            calNum = calNum + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + lAug;
+        }
+        if (calMonth == 10) {
+            calNum = calNum + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + lAug + lSep;
+        }
+        if (calMonth == 11) {
+            calNum = calNum + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + lAug + lSep + lOct;
+        }
+        if (calMonth == 12) {
+            calNum = calNum + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + lAug + lSep + lOct + lNov;
+        }
+
+        int calcDay;
+
         int day;
         int month;
         int year;
@@ -62,8 +116,6 @@
              << " gelijk aan " << ageMonths / 12 << " jaar en " 
              << ageMonths % 12 << " maanden." << endl;
 
-        //cout << ctime(&currentDate);
-
         if ( month == currentMonth && day == currentDay ) {
             cout << "Gefeliciteerd, met uw verjaardag!" << endl;
         }//Felicitaties voor de jarige
@@ -72,8 +124,70 @@
             cout << "Helaas, u voldoet niet aan de leeftijdseisen van de univer"
                  << "siteit." << endl;
             return 1;
-        }//Afwijzing van de geweigerde.
+        }//Afwijzing van de gebruiker.
         else {
+            if (month == 1) {
+                calcDay = year * 365 + day;
+            }
+            if (month == 2) {
+                calcDay = year * 365 + lJan + day;
+            }
+            if (month == 3) {
+                calcDay = year * 365 + lJan + lFeb + day;
+            }
+            if (month == 4) {
+                calcDay = year * 365 + lJan + lFeb + lMar + day;
+            }
+            if (month == 5) {
+                calcDay = year * 365 + lJan + lFeb + lMar + lApr + day;
+            }
+            if (month == 6) {
+                calcDay = year * 365 + lJan + lFeb + lMar + lApr + lMay + day;
+            }
+            if (month == 7) {
+                calcDay = year * 365 + lJan + lFeb + lMar + lApr + lMay + lJun + day;
+            }
+            if (month == 8) {
+                calcDay = year * 365 + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + day;
+            }
+            if (month == 9) {
+                calcDay = year * 365 + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + lAug + day;
+            }
+            if (month == 10) {
+                calcDay = year * 365 + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + lAug + lSep + day;
+            }
+            if (month == 11) {
+                calcDay = year * 365 + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + lAug + lSep + lOct + day;
+            }
+            if (month == 12) {
+                calcDay = year * 365 + lJan + lFeb + lMar + lApr + lMay + lJun + lJul + lAug + lSep + lOct + lNov + day;
+            }
+            calcDay = calcDay + year/4 - year/100 + year/400;
+            if ((calcDay - calNum) % 7 == 0) {
+               birthDay = 'd';
+               birthDay2 = 'i';
+            }
+            if ((calcDay - calNum) % 7 == 1) {
+               birthDay = 'w';
+            }
+            if ((calcDay - calNum) % 7 == 2) {
+               birthDay = 'd';
+               birthDay2 = 'o';
+            }
+            if ((calcDay - calNum) % 7 == 3) {
+               birthDay = 'v';
+            }
+            if ((calcDay - calNum) % 7 == 4) {
+               birthDay = 'z';
+               birthDay2 = 'a';
+            }
+            if ((calcDay - calNum) % 7 == 5) {
+               birthDay = 'z';
+               birthDay2 = 'o';
+            }
+            if ((calcDay - calNum) % 7 == 6) {
+               birthDay = 'm';
+            }
             cout << "Vul de eerste letter in van de dag waarop u geboren bent, "
                  << "gebruik 1 letter." << endl;
             cin >> weekDay;
@@ -81,15 +195,15 @@
                 cout << "Vul ook de tweede letter van de dag in." << endl;
                 cin >> weekDay2;
                 if ( weekDay2 == birthDay2 ) {
-                    cout << "Dat is incorrect." << endl;
-                    return 1;
+                    cout << "Correct!" << endl;
                 }
                 else {
-
+                    cout << "Dat is incorrect." << endl;
+                    return 1;                    
                 }
             }
             else if ( weekDay == birthDay ) {
-                
+                cout << "Correct!" << endl;
             }
             else {
                 cout << "Dat is incorrect." << endl;
