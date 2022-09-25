63391ddbba20eefc6d243fcf28058215d2baa36b
Thijs Vijgeboom <avijgeboom@hotmail.com>
2022-09-25 14:33:29 +0200

    EINDELIJK!

--- assignment_1.cpp
+++ assignment_1.cpp
@@ -276,18 +276,20 @@
         int C1 = ( rand() % 159 ) - 39;
         float F1 = 9 * (float) C1 / 5 + 32;
         float tF1 = F1 * 100;
-        if ((int) tF1 < tF1 - 0,5 ) {
+        if ((int) tF1 < tF1 - 0.5 ) {
             tF1++;
         }
-        float dF1 = ((int) tF1) / 100;
+        float dF1 = (int) tF1;
+        dF1 = dF1 / 100;
 
         int F2 = ( rand() % 159 ) - 39;
         float C2 = ( (float) F2 - 32 ) * 5 / 9;
         float tC2 = C2 * 100;
-        if ((int) tC2 < tC2 - 0,5 ) {
+        if ((int) tC2 < tC2 - 0.5 ) {
             tC2++;
         }
-        float dC2 = ((int) tC2) / 100;
+        float dC2 = (int) tC2;
+        dC2 = dC2 / 100;
         cout << "Hoeveel graden Fahrenheit staat gelijk aan " << C1
              << " graden Celsius? Typ een geheel getal, toets daarna ENTER. "
              << endl;
@@ -353,7 +355,7 @@
             }
         }
         else {
-            cout << "Graag zien wij u volgend jaar terug bij een van de bèta"
+            cout << "Graag zien wij u volgend jaar terug bij een van de beta"
                  << " studies aan de universiteit van Leiden." << endl;
             return 1; 
         }
--- test.cpp
+++ test.cpp
@@ -2,49 +2,58 @@
     using namespace std;
 
     int main() {
+        srand(7);
         bool dbg = true;
         bool Q1 = true;
         bool Q2 = true;
-        int C1 = 1;//( rand() % 159 ) - 39;
-        float F1 = 9 * (float) C1 / 7 + 32;
-
-        float tF1 = F1 * 100;
-        float dF1 = tF1 / 100;
-        if( dbg ) {
-            cout << F1 << endl;
-            cout << tF1 << endl;
-            cout << dF1 << endl;
-        }
-
-        int F2 = ( rand() % 159 ) - 39;
-        float C2 = ( F2 - 32 ) * 5 / 9;
-
-        float tF2 = F2 * 100;
-        float dF2 = tF2 / 100;
-        if( dbg ) {
-            cout << F1 << endl;
-            cout << tF1 << endl;
-            cout << dF1 << endl;
-        }
 
         int x;
         int y;
+
+        int C1 = ( rand() % 159 ) - 39;
+        float F1 = 9 * (float) C1 / 7 + 32;
+        if(dbg) cout << F1 << endl;
+        float tF1 = F1 * 100;
+        if(dbg) cout << tF1 << endl;
+        if ((int) tF1 < tF1 - 0.5 ) {
+            tF1++;
+        }
+        if(dbg) cout << tF1 << endl;;
+        float dF1 = (int) tF1;
+        if(dbg) cout << dF1 << endl;
+        dF1 = dF1 / 100;
+        if(dbg) cout << dF1 << endl;
+
+        int F2 = ( rand() % 159 ) - 39;
+        float C2 = ( (float) F2 - 32 ) * 5 / 9;
+        float tC2 = C2 * 100;
+        if ((int) tC2 < tC2 - 5 ) {
+            tC2++;
+        }
+        float dC2 = (int) tC2;
+        dC2 = dC2 / 100;
         cout << "Hoeveel graden Fahrenheit staat gelijk aan " << C1
-             << " graden Celsius? Type een geheel getal, toets daarna ENTER. "
+             << " graden Celsius? Typ een geheel getal, toets daarna ENTER. "
              << endl;
         cin >> x;
-        if ( x <= dF1 - 1 || x >= dF1 + 1) {
-            Q1 = false;
-        }
 
+        if (( x < dF1 - 1 ) || ( x > dF1 + 1 )) {
+            Q1 = false;
+            cout << "Helaas is dit niet het juiste antwoord." << endl;
+        }
         cout << dF1 << " is het goede antwoord." << endl;
 
-        if (!Q1 && !Q2) {
-            char antw;
-            cout << "Uw antwoorden wijken te veel af van de juiste antwoorden."
-                 << " Helaas bent u niet geschikt voor een beta-studie."
-                 << endl;
+        cout << "Hoeveel graden Celsius staat gelijk aan " << F2
+             << " graden Fahrenheit? Typ een geheel getal, toets daarna ENTER. "
+             << endl;
+        cin >> y;
+
+        if (( y < dC2 - 1 ) || ( y > dC2 + 1 )) {
+            Q2 = false;
+            cout << "Helaas is dit niet het juiste antwoord." << endl;
         }
+        cout << dC2 << " is het goede antwoord." << endl;
+
         return 0;
     }       
         
