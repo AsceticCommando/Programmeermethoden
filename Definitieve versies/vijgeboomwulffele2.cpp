e67a12fbb76d957bf315b92b18d73f93485e0343
AsceticCommando <avijgeboom@hotmail.com>
2022-11-13 16:31:09 +0100

    Poging tot implementatie van dewereld en oplossing Ook karakter invoer verbeteringen m.b.v. strings

---
+++ Definitieve versies/vijgeboomwulffele2.cpp
@@ -0,0 +1,308 @@
+    //Infoblok voor programmeurs
+    //Compiler:          g++.exe (Rev1, Built by MSYS2 project) 12.2.0
+    //Programmeerpgave:                                              1
+    //Gemaakt op:                                           26-09-2022
+    //Namen:                   Vijgeboom, Thijs;       Wulffele, Lotte
+    //Studentnummers:                   2648261;               3661814
+    //Jaar van aankomst:                   2019;                  2022
+    //Studierichting:                  Biologie;              Wiskunde
+    //Gebruikte IDE:         Visual Studio Code;          Sublime Text
+
+    #include<iostream>
+    #include<fstream>
+    #include<string>
+    using namespace std;
+
+    void info () { 
+        cout << "+---------------------------------------------------"
+             << "---+" << endl << "| Programmeermethoden; tweede prog"
+             << "rammeeropgave        |" << endl << "| Gemaakt op;   "
+             << "       17-10-2022                      |" << endl
+             << "| Gemaakt door:      Thijs Vijgeboom en Lotte Wulffe"             
+             << "le |" << endl << "| Studentnummers:    2648261      " 
+             << "      3661814        |" << endl << "| Jaar van aanko"
+             << "mst: 2019               2022           |" << endl
+             << "| Studierichting:    Biologie           Wiskunde    "
+             << "   |" << endl << "+---------------------------------"
+             << "---------------------+" << endl << endl << "Goedenda"
+             << "g; u heeft een programma opgestart dat zowel voor co" 
+             << "deren als voor decoderen kan worden"
+             << " gebruikt." << endl << "Zo"
+             << " zal ik u wat vragen stellen om helder te krijgen "
+             << "wat u van mij verwacht" << endl << "Wat u van mij ku"
+             << "nt verwachten?" << endl << "Ik zal coderen, decodere"
+             << "n, verloren pincodes terugzoeken en u Lachrel-gerela"
+             << "teerde informatie verschaffen omtrent getallen die i"
+             << "n ene te coderen bestand voorkomen." << endl << "Wel"
+             << "kom!" << endl << endl;
+    }
+
+    int invertPin (int pin) {
+        int invertedPin = 0;
+        for (int p = 0; p < 4; p++) {
+            invertedPin = invertedPin * 10 + pin % 10;
+            pin = pin / 10;
+        }
+        return invertedPin;
+    }//pincode omdraaien
+
+    int invertNum (int num) {
+        int invertedNum = 0;
+        while (num > 0) {
+            invertedNum = invertedNum * 10 + (num % 10);
+            num /= 10;
+        }
+        return invertedNum;
+    }//getallen omdraaien voor Lychrel
+
+    bool checkPin (int pin) {
+        if ( pin > 0 && pin < 10000 ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }//Controle geldigheid pincode
+
+    bool checkLychrel (int num, int & counter) {
+        int storedNum = num;
+        int invertedNum = invertNum(num);
+        if (num == 0) {
+            return false;
+        }
+        while (storedNum != invertedNum) {
+            counter++;
+            if (storedNum < INT_MAX / 10 
+                && invertedNum < INT_MAX / 10) {
+                storedNum += invertedNum;
+                invertedNum = invertNum(storedNum);
+            }
+            else {
+                return true;
+            }//INTMAX bereikt voor een palindroom gevonden is
+        }//Zolang het getal geen palindroom is,
+         //getal omkeren en optellen
+        return false;
+    }//Lychrel-test
+
+    int encryptFile (int pin, ifstream & input, ofstream & output, 
+                     bool decrypt = false, bool testing = false) {
+        char nextKar = input.get(); //Pakt volgende char uit de stream
+        char kar;               //Voor uit te voeren operaties op char
+        char decryptedKar;             //Bewerkte char voor decodering
+        char nextDecryptedKar;           //Bewerkte volgende char voor 
+                                         //getalherkenning
+
+        int pinCount = 0;                   //Houdt de huidige pin bij
+        int pinnum = pin;                //counter voor opslag pincode
+        
+        int foundNum = 0;                 //Slaat gevonden getallen op
+        int lastPin = 0;         //Bewaart de laatst gebruikte pincode
+
+        bool encounteredNum = false;  //Geeft aan of een getal bestaat
+        bool pinFound = false;     //Geeft aan of er een nieuwe pin is
+
+        char firstKar;            //Opslag variabelen voor 't','h','e'
+        char secondKar;
+        char lastKar;
+
+        int countThe = 0;             //Houdt de hoeveelheid the's bij
+
+        while (!input.eof()) {
+            kar = nextKar;                //Hier worden de volgende te
+            nextKar = input.get();        //bewerken karakters ingezet
+            
+            if (kar == '\r' || kar == '\n') {
+                pinnum = pin;
+                pinCount = 0;
+                if (!testing) {
+                    output.put(kar);
+                }
+                pinFound = false;
+            }//carriage return/endline resetten
+            else if (kar == '\t') {
+                if (!testing) {
+                    output.put(kar);
+                }
+            }//tabs overslaan
+            else {
+                int currNum;
+                if (pinFound) {
+                    pinCount++;
+                    currNum = lastPin % 10;
+                    pinFound = false;
+                }//Bewaart de laatst gebruikte pin voor volgende char.
+                else {
+                    pinCount++;
+                    currNum = pinnum % 10;
+                }//Houdt de huidige pin bij
+                decryptedKar = (kar - currNum + 63) % 95 + 32;
+                //Te gebruiken voor vinden van getallen.
+                if (!decrypt && !testing) {
+                    output.put((kar + currNum - 32) % 95 + 32);
+                }
+                else if (decrypt && !testing) {
+                    output.put(decryptedKar);
+                }
+                else {
+                    firstKar = secondKar;
+                    secondKar = lastKar;
+                    lastKar = (kar - currNum + 63) % 95 + 32;
+                    if ((firstKar == 't' || firstKar == 'T')
+                        && (secondKar == 'h' || secondKar == 'H')
+                        && (lastKar == 'e' || lastKar == 'E')) {
+                        countThe++;
+                    }//Houdt gevonden "the's" bij
+                }
+                if (pinCount == 4) {
+                    pinnum = pin;
+                    pinCount = 0;
+                }//Reset pin indien 4 is berijkt
+                else {
+                    pinnum /= 10;
+                }//Anders delen door 10
+                if (kar >= '0' && kar <= '9' && !decrypt) {
+                    encounteredNum = true;
+                    if ((foundNum < INT_MAX / 10) || 
+                        (foundNum == INT_MAX / 10 && 
+                        (kar - '0') <= INT_MAX % 10)) {
+                        foundNum = foundNum * 10 + (kar - '0');
+                    }//Indien het gevonden nummer geen intmax bereikt
+                     //Voeg het volgende getal toe
+                    else {
+                        foundNum = INT_MAX;
+                    }//Anders nemen we aan dat het getal intmax is.
+                }//Vindt getallen
+                if ((nextKar < '0' || nextKar > '9')  
+                    && encounteredNum == true && !decrypt) {
+                    encounteredNum = false;
+                    int counter = 0;      //Houdt het aantal iteraties
+                                          //bij voor Lychrel getallen
+                    if (checkPin(foundNum)) {
+                        lastPin = pinnum;
+                        pinnum = invertPin(foundNum);
+                        pin = pinnum;
+                        pinCount = 3;
+                        pinFound = true;
+                    }//Kijkt of het getal voldoet als pincode
+                    if (!checkLychrel(foundNum, counter)) {
+                        cout << "Het getal " << foundNum << " is geen"
+                             << " Lychrel getal. Het vormt na " 
+                             << counter << " iteraties een palind"
+                             << "room." << endl;
+                    }//Kijkt of het een Lychrel getal is.
+                    else {
+                        cout << "Het gevonden getal " << foundNum
+                             << " wordt groter dan " << INT_MAX 
+                             << " na " << counter << " iteraties zond"
+                             << "er een palindroom te vormen en is du"
+                             << "s vermoedelijk een Lychrel getal." 
+                             << endl;
+                    }//Confirmatie van vermoedelijk Lychrel getal
+                    foundNum = 0;
+                }//Vindt einde van een getal
+                if (decryptedKar >= '0' && decryptedKar <= '9' 
+                    && decrypt) {
+                    encounteredNum = true;
+                    int nextPin = pinnum % 10;
+                    nextDecryptedKar = ((nextKar - (nextPin % 10)) 
+                                        + 63) % 95 + 32;
+                    if ((foundNum < INT_MAX / 10) || 
+                        (foundNum == INT_MAX / 10 && 
+                        (decryptedKar - '0') <= INT_MAX % 10)) {
+                        foundNum = foundNum * 10 
+                                   + (decryptedKar - '0');
+                    }
+                    else {
+                        foundNum = INT_MAX;
+                    }
+                }//Dezelfde functie voor het getal bij decoderen
+                if ((nextDecryptedKar < '0' || nextDecryptedKar > '9')  
+                    && encounteredNum == true && decrypt) {
+                    encounteredNum = false;
+                    int counter = 0;
+                    if (checkPin(foundNum)) {
+                        lastPin = pinnum;
+                        pinnum = invertPin(foundNum);
+                        pin = pinnum;
+                        pinCount = 3;
+                        pinFound = true;
+                    }
+                    foundNum = 0;
+                }//Vindt einde van het getal bij decoderen
+            }
+        }//Loop zolang end of file niet bereikt is.
+        input.close();
+        output.close();
+        return countThe;
+    }//Functie voor (de)coderen van de file, parameters zijn als volgt
+     //pin: te gebruiken voor (de)coderen
+     //if-/ofstream, bestand verwijzingen
+     //decrypt: true = decoderen, false = coderen
+     //testing: voorkomt wegschrijven van karakters voor lostPassword
+     //Geeft het aantal "the's" als return value
+    
+    int lostPassword (string inputname, string outputname) {
+        int maxThe = 0;  //Meest bereikte hoeveelheid "The's" in tekst
+        int foundPin = 0;                    //De bijbehorende pincode
+        for (int i = 0; i <= 9999; i++) {
+            ifstream input(inputname);
+            ofstream output(outputname);
+            int count = encryptFile(invertPin(i), input, output, true,
+                                    true);
+            if (count > maxThe) {
+                maxThe = count;
+                foundPin = i;
+            }//Wanneer meer "the's" gevonden zijn, wordt de nieuwe pin
+        }//Loopt van 0 tot en met 9999 voor vinden van geldige pincode
+        ifstream encrypted(inputname);   //Namen voor de uiteindelijke
+        ofstream result(outputname);     //In- en uitvoer bestanden
+        encryptFile(invertPin(foundPin), encrypted, result, true, 
+                    false);
+        return foundPin;
+    }//Bij verloren pincode
+
+    int main () {
+        char toBe;                                   //Welke wordt het
+        bool decrypt;       //Wil je coderen(false) of decoderen(true)
+        int pincode;                             //Voor invoer pincode
+        info();
+        cout << "Wilt u een bestand coderen of decoderen? (C/D):  ";
+        cin >> toBe;
+        if (toBe == 'C' || toBe == 'c') {
+            decrypt = false;
+        }
+        else if (toBe == 'D' || toBe == 'd') {
+            decrypt = true;
+        }
+        else {
+            cout << "Dat was geen optie, uit voorzorgsmaatregelen slu"
+                 << "it het programma nu af." << endl;
+            return 1;
+        }//afsluiten bij invoer van foute char
+        string inputname;     //Namen voor de in- en uitvoer bestanden
+        string outputname;
+        cout << "Voer de naam in van het te (de)coderen bestand: ";
+        cin >> inputname;
+        cout << "Voer de naam in van het doelbestand: ";
+        cin >> outputname;
+        ifstream input(inputname);     //Openen van betreffende in- en
+        ofstream output(outputname);   //uitvoer bestanden
+        cout << "Voer een geldige pincode in voor het (de)coderen van"
+             << " het bestand, als u uw pincode niet weet, voer dan -"
+             << "1 in: ";
+        cin >> pincode;
+        if (checkPin(pincode)) {
+            encryptFile(invertPin(pincode), input, output, decrypt);
+        }//Indien pincode geldig is, (de)coderen
+        else if (pincode == -1 && decrypt) {
+            cout << "De gevonden pincode is: " 
+                 << lostPassword(inputname, outputname) << endl;
+        }//Niet onthouden? dan deze proberen
+        else {
+            cout << "Dit is geen geldige optie, uit voorzorgsmaatrege"
+                 << "len sluit het programma nu af." << endl;
+            return 1;
+        }//Dit mag niet
+        return 0;
+    }//main
---
+++ PR3.cpp
@@ -0,0 +1,265 @@
+    //Infoblok voor programmeurs
+    //Compiler:          g++.exe (Rev1, Built by MSYS2 project) 12.2.0
+    //Programmeerpgave:                                              1
+    //Gemaakt op:                                           26-09-2022
+    //Namen:                   Vijgeboom, Thijs;       Wulffele, Lotte
+    //Studentnummers:                   2648261;               3661814
+    //Jaar van aankomst:                   2019;                  2022
+    //Studierichting:                  Biologie;              Wiskunde
+    //Gebruikte IDE:         Visual Studio Code;          Sublime Text
+    
+    #include <iostream>
+    #include <string>
+    #include <fstream>
+    using namespace std;
+    
+    //klasse aanmaken
+    class Puzzel {
+        private:
+            bool dewereld[20][20];  // daadwerkelijke MAX ipv 'MAX'; is de bedoeling toch?
+            bool oplossing[20][20]; // daadwerkelijke MAX ipv 'MAX'; in de bedoeling toch?
+            int hoogte, breedte;
+            char aan, uit;
+            float percentage;
+        public:
+            Puzzel() {
+                hoogte = 5;
+                breedte = 5;
+                aan = 'X';
+                uit = '.';
+                vulRandom();
+            } // constructor
+            void drukAf() {
+                for (int i = 0; i < hoogte; i++) {
+                    for (int j = 0; j < breedte; j++) {
+                        if (dewereld[i][j])
+                            cout << ' ' << aan;
+                        else
+                            cout << ' ' << uit;
+                    }// for j
+                    cout << endl;
+                } // for i
+            }// functie drukaf
+            void losOp() {
+                for (int i = 0; i < hoogte; i++) {
+                    for (int j = 0; j < breedte; j++) {
+                        if (oplossing[i][j])
+                            cout << ' ' << aan;
+                        else
+                            cout << ' ' << uit;
+                    }// for j
+                    cout << endl;
+                } // for i
+            }
+            void vulRandom() {
+
+            }
+            void maakSchoon() {
+                for (int i = 0; i < hoogte; i++) {
+                    for (int j = 0; j < breedte; j++) {
+                        dewereld[i][j] = false;
+                    }
+                }
+            }
+            void zetPercentage() {
+                int correct = 0;
+                for (int i = 0; i < hoogte; i++) {
+                    for (int j = 0; j < breedte; j++) {
+                        if (dewereld[i][j] == oplossing[i][j]) {
+                            correct++; 
+                        }
+                    }
+                }
+                percentage = correct/(hoogte*breedte)
+            }
+            void zetParameters() {
+                cout << "U kunt hier uw eigen parameters kiezen:" << endl;
+                cout << "Type als geheel getal (2-20) de door uw gewenste breedte van het speelvlak in." << endl;
+                while(b >= '0' &&) {
+                }
+                cout << "Type als geheel getal (2-20) de door uw gewenste hoogte van het speelvlak in." << endl;
+                cin >> hoogte;
+                cout << "Welk symbool wilt u gebruiken voor een lampje dat uit is?" << endl;
+                cin >> uit;
+                cout << "Welk symbool wilt u gebruiken voor een lampje dat aan is?" << endl;
+                cin >> aan;
+                // vraag en invoer fietsband-optie of gewoon speelveld (j/n)
+                // een pen (0/1/2) 0: bij lopen blijven lampen gelijk 1: lopen doet lampen aan 2: lopen doet lampen uit
+            }
+            void doeZet(string zet) {
+                char letter = zet[0];
+                char nummer = zet[1];
+                if (((letter >= 'A' && letter < 'A' + breedte) || (letter >= 'a' && letter < 'a' + breedte)) && nummer > '0' && nummer <= '0' + hoogte) {
+                    int posi = letter - 'A';
+                    int posj = ('0' + hoogte - nummer);
+                    for (int i = -1; i <= 1; i++) {
+                       for (int j = -1; j <= 1; j++) {
+                           if (posi + i >= 0 && posi + i <= breedte - 1 && posj + j >= 0 && posj + j <= hoogte - 1) {
+                               dewereld[posi + i][posj + j] = !dewereld[posi + i][posj + j];
+                           }
+                       }
+                    }
+                }
+                else {
+                    cout << "Deze zet is niet geldig. Gebruik schaaknotatie om een zet te doen (i.e. [A1])." << endl;
+                }
+            }
+            // functie stoppen
+            //  programma sluit af
+            
+            // functie terug
+            // stuurt gebruiker terug naar hoofdmenu
+            
+            // functie maakschoon
+            // alle lampen van het speelveld gaan uit
+            
+            // functie random
+            // speelveld wordt random gevult door willekeurige lampen aan en uit te doen; hoeft geen oplosbare puzzel op te leveren
+            
+            // functie toggle
+            // laat aan lampje uit, en uit lampje aan gaan
+            // is geen zet!! enkel het lampje zelf reageerd
+            
+            // functie genereer
+            // geen functie op ingevoerde getal uit te voeren
+    };
+    
+    // submenu Parameters
+    int genereerSpeelveld() {
+        int g;
+        cout << "Kies uw moeilijkheidsgraad. Gelieve een geheel, positief getal in te voeren." << endl;
+        cin >> g;
+        return g;
+    }
+    
+    // functie oplossen
+    // lost 5x5 functie op
+    // aantal gedane zetten en lampjes die aan zijn afdrukken naast speelveld
+    
+    // functie leesGetal
+    int getal = 0;
+    void leesGetal() {
+        char x = cin.get();
+        while (x == '\n') {
+            x = cin.get(); // naar volgende char bij \n
+        }
+        while (x < '0' || x > '9') {
+            x = cin.get(); // naar volgende char bij niet-getal
+        }
+        while (getal < 1000 && 0 > x && x < 9) { // checken of getal niet boven de bovengrens komt vinden en returnen getal achtereenvolgende nummers
+            getal = (getal * 10) + x;              // getal vormen
+            x = cin.get();
+        }
+        while (x > -1 && x < 10) {
+            x = x * 10;
+            getal = x;
+            x = cin.get();
+        }
+    }
+    
+    // functie random getal
+    //  geeft random getal tussen 0 en 999
+    int randomgetal() {
+        static int getal = 42;
+        getal = (221 * getal + 1) % 1000;
+        return getal;
+    } // randomgetal
+
+    // submenu tekenen
+    void tekenMenu() { // moet ongetwijfeld geen 'int' zijn, maar welke functie wel?
+        char choice;
+        bool back = false;
+        while (!back) {
+            cout << "[T]erug, s[C}hoon, [R]andom, t[O]ggle, [G]enereer" << endl;
+            cout << "Gebruik [WASD] om de cursor te bewegen." << endl;
+            cin >> choice;
+            switch (choice) {
+                case 'T': case 't':
+                    back = true;
+                    break;
+                case 'W': case 'w':
+                    //omhoog
+                case 'A': case 'a':
+                    //links
+                case 'S': case 's':
+                    //omlaag
+                case 'D': case 'd':
+                    //rechts
+                case 'C': case 'c':
+                    cout << "schoon" << endl;
+                case 'R': case 'r':
+                    cout << "random" << endl;
+                case 'O': case 'o':
+                    cout << "toggle" << endl;
+                case 'G': case 'g':
+                    cout << "genereer" << endl;
+            }
+        }
+    }
+    
+    // submenu puzzelen
+    void puzzelMenu(Puzzel mijnPuzzel) { // moet ongetwijfeld geen 'int' zijn, maar welke functie wel?
+        char choice;
+        bool back = false;
+        while (!back) {
+            cout << "[T]erug, [V]olg, [O]plossen van 5x5 puzzel, [A]fspelen oplossing, [Z]et doen" << endl;
+            cin >> choice;
+            switch (choice) {
+                case 'T': case 't':
+                    back = true;
+                    break;
+                case 'V': case 'v':
+                    //volg
+                case 'O': case 'o':
+                    //los op
+                case 'A': case 'a':
+                    //los op en laat zien
+                case 'Z': case 'z':
+                    cout << "Doe een zet doormiddel van schaaknotatie (i.e. [A1])." << endl;
+                    string zet;
+                    char z = cin.get();
+                    bool gevonden = false;
+                    while (z != '\n') {
+                        if ((z >= 'A' && z <= 'Z') || (z >= 'a' && z <= 'z') && !gevonden) {
+                            zet += z;
+                            gevonden = true;
+                        }
+                        else if (gevonden == true && (z >= '1' && z <= '9')) {
+                            zet += z;
+                        }
+                        z = cin.get();
+                    }
+                    mijnPuzzel.doeZet(zet);
+                    //doe een zet
+        }
+        }
+    }
+
+    void menu() {
+        Puzzel mijnPuzzel;
+        char choice;
+        bool exit = false;
+        while (!exit) {
+            cout << "[T]ekenen, p[U]zzelen, [P]arameters, [S]toppen" << endl;
+            cin >> choice;
+            switch (choice) {
+                case 'T': case 't':
+                    tekenMenu();
+                    break;
+                case 'U': case 'u':
+                    puzzelMenu(mijnPuzzel);
+                    break;
+                case 'P': case 'p':
+                    mijnPuzzel.zetParameters();
+                    break;
+                case 'S': case 's':
+                    exit = true;
+                    break;
+            }
+        }
+    }
+
+    int main() {
+        menu();
+        return 0;
+    }
